70c9ab897f904acff9c8d1d15c8e0719ee1e9499
Coder7Raj <alwaysraj733@gmail.com>
2026-07-30 03:59:09 +0600

    added schema's functionalities for user dashboard

---
+++ backend/src/modules/dashboard/dashboard.repository.js
@@ -0,0 +1,103 @@
+const prisma = require("../../config/prisma.js");
+
+const countUserUrls = async (userId) => {
+  return prisma.urls.count({
+    where: {
+      user_id: BigInt(userId),
+      deleted_at: null,
+    },
+  });
+};
+
+const countActiveUrls = async (userId) => {
+  return prisma.urls.count({
+    where: {
+      user_id: BigInt(userId),
+      deleted_at: null,
+      status: "ACTIVE",
+    },
+  });
+};
+
+const countExpiredUrls = async (userId) => {
+  return prisma.urls.count({
+    where: {
+      user_id: BigInt(userId),
+      deleted_at: null,
+      expires_at: {
+        lt: new Date(),
+      },
+    },
+  });
+};
+
+const countDeletedUrls = async (userId) => {
+  return prisma.urls.count({
+    where: {
+      user_id: BigInt(userId),
+      NOT: {
+        deleted_at: null,
+      },
+    },
+  });
+};
+
+const countClicks = async (userId) => {
+  return prisma.clicks.count({
+    where: {
+      urls: {
+        user_id: BigInt(userId),
+      },
+    },
+  });
+};
+
+const countTodayClicks = async (userId, today) => {
+  return prisma.clicks.count({
+    where: {
+      clicked_at: {
+        gte: today,
+      },
+      urls: {
+        user_id: BigInt(userId),
+      },
+    },
+  });
+};
+
+const countWeekClicks = async (userId, week) => {
+  return prisma.clicks.count({
+    where: {
+      clicked_at: {
+        gte: week,
+      },
+      urls: {
+        user_id: BigInt(userId),
+      },
+    },
+  });
+};
+
+const countMonthClicks = async (userId, month) => {
+  return prisma.clicks.count({
+    where: {
+      clicked_at: {
+        gte: month,
+      },
+      urls: {
+        user_id: BigInt(userId),
+      },
+    },
+  });
+};
+
+module.exports = {
+  countUserUrls,
+  countActiveUrls,
+  countExpiredUrls,
+  countDeletedUrls,
+  countClicks,
+  countTodayClicks,
+  countWeekClicks,
+  countMonthClicks,
+};
