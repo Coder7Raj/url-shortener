b0def5158ac08223a9e427e1609fbb472ca0cfcf
Coder7Raj <alwaysraj733@gmail.com>
2026-08-05 09:51:38 +0600

    added constants file for base api and routes

---
+++ frontend/src/constants/routes.js
@@ -0,0 +1,16 @@
+export const ROUTES = {
+  HOME: "/",
+
+  LOGIN: "/login",
+  REGISTER: "/register",
+
+  DASHBOARD: "/dashboard",
+
+  URLS: "/dashboard/urls",
+  ANALYTICS: "/dashboard/analytics",
+
+  QR: "/dashboard/qr",
+  SESSIONS: "/dashboard/sessions",
+
+  PROFILE: "/dashboard/profile",
+};
