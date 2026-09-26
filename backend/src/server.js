6e89bc24e3fbca600545fcafc217ff74fe4a1fc6
Coder7Raj <alwaysraj733@gmail.com>
2026-09-26 15:16:17 +0600

    updated backend for render deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 const env = require("./config/env.js");
 
-app.listen(env.port, () => {
+app.listen(env.port, "0.0.0.0", () => {
   console.log(`Server running on port ${env.port} (${env.nodeEnv})`);
 });
